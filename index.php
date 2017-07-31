42ef04a4802cff5579a669e1ea145d3f50990178
haajidheere <jilaa.me@gmail.com>
2017-07-31 22:31:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_REQUEST['hub_challenge'])){
+    $challenge = $_REQUEST['hub_challenge'];
+    $token = $_REQUEST['hub_verify_token'];
+}
+if($token == "myCustomeToken123") {
+    echo $challenge;
+}
+
+?>
